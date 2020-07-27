4e3458d2bb6df3f5a02cd26928c4f89d649d31b5
ysjee141 <ysjee141@gmail.com>
2020-07-27 01:30:07 +0000

    ysjee141 published a site update

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,43 @@
+var store = [{
+        "title": "Test - Article with Long TOC",
+        "excerpt":"Article with long TOC.     TeXt Heading   TeXt Heading   TeXt Heading   TeXt Heading   TeXt Heading   TeXt Heading   TeXt Heading   Very Very Very Very Very Very Very Very Very Very Very Very Very Extremely Completely Extraordinary Long Long Long Long Title   TeXt Heading   TeXt Heading   TeXt Heading   TeXt Heading   TeXt Heading   TeXt Heading   TeXt Heading   TeXt Heading   Very Very Very Very Very Very Very Very Very Very Very Very Very Extremely Completely Extraordinary Long Long Long Long Title   TeXt Heading   TeXt Heading   TeXt Heading   TeXt Heading   TeXt Heading   TeXt Heading   TeXt Heading   TeXt Heading   TeXt Heading   Very Very Very Very Very Very Very Very Very Very Very Very Very Extremely Completely Extraordinary Long Long Long Long Title   TeXt Heading   TeXt Heading  ","categories": ["Spring"],
+        "tags": ["Test"],
+        "url": "/spring/article-with-long-toc/",
+        "teaser": "/assets/img/coding.jpg"
+      },{
+        "title": "Test - Article with Lots of Images",
+        "excerpt":"TeXt Theme - Cover        TeXt Theme - Layouts      Skin: Default      Skin: Dark      Skin: Forest      Skin: Ocean      Skin: Chocolate      Skin: Orange     ","categories": ["No Category"],
+        "tags": ["Test"],
+        "url": "/no%20category/article-with-lots-images/",
+        "teaser": "/assets/img/coding.jpg"
+      },{
+        "title": "Test - Very Very Very Very Very Very Very Very Very Very Very Very Very Extremely Completely Extraordinary Long Long Long Long Title",
+        "excerpt":"Article With Very Long Title.  ","categories": ["No Category"],
+        "tags": ["Test"],
+        "url": "/no%20category/very-long-title/",
+        "teaser": "/assets/img/coding.jpg"
+      },{
+        "title": "Jekyll - Horizontal Rules",
+        "excerpt":"    * * *     ***     *****     - - -     ---------------------------------------  ","categories": ["No Category"],
+        "tags": ["Jekyll"],
+        "url": "/no%20category/horizontal-rules/",
+        "teaser": "/assets/img/coding.jpg"
+      },{
+        "title": "Jekyll - Footnote",
+        "excerpt":"Here is a footnote reference,1 and another.2     markdown:   Here is a footnote reference,[^1] and another.[^longnote]  [^1]: Here is the footnote.  [^longnote]: Here’s one with multiple blocks.      Subsequent paragraphs are indented to show that they belong to the previous footnote.                 Here is the footnote. &#8617;                  Here’s one with multiple blocks.         Subsequent paragraphs are indented to show that they belong to the previous footnote. &#8617;           ","categories": ["No Category"],
+        "tags": ["Jekyll"],
+        "url": "/no%20category/footnote/",
+        "teaser": "/assets/img/coding.jpg"
+      },{
+        "title": "Jekyll - Definition",
+        "excerpt":"   kramdown   A Markdown-superset converter   Maruku   Another Markdown-superset converter     markdown:   kramdown : A Markdown-superset converter  Maruku :     Another Markdown-superset converter  ","categories": ["No Category"],
+        "tags": ["Jekyll"],
+        "url": "/no%20category/definition/",
+        "teaser": "/assets/img/coding.jpg"
+      },{
+        "title": "Jekyll - Blockquotes",
+        "excerpt":"ssdkjlfsdadfasdfssdkjlfsdadfasdfssdkjlfsdadfasdfssdkjlfsdadfasdfssdkjlfsdadfasdfssdkjlfsdadfasdfssd kjlfsdadfasdfssdkjlfsdadfasdfssdkjlfsdadfasdfssdkjlfsdadfasdf ssdkjlfsdadfasdfssdkjlfsdadfasdfssdkjlfsdadfasdfssdkjlfsdadfasdfssdkjlfsdadfasdfssdkjlfsdadfasdfssdkjlfsdadfasdfssdkjlfsdadfasdfssdkjlfsdadfasdfssdkjlfsdadfasdfssdkjlfsdadfasdfssdkjlfsdadfasdf ssdkjlfsdadfasdfssdkjlfsdadfasdfssdkjlfsdadfasdfssdkjlfsdadfasdfssdkjlfsdadfasdfssdkjlfsdadfasdfssdkjlfsdadfasdfssdkjlfsdadfasdf     “There is nothing either good or bad, but thinking makes it so.”     —Hamlet in Hamlet      markdown:   &gt; “There is nothing either good or bad, but thinking makes it so.” &gt; &gt; —Hamlet in *Hamlet*     ssdkjlfsdadfasdf     “From women’s eyes this doctrine I derive:     They sparkle still the right Promethean fire;     They are the books, the arts, the academes,     That show, contain, and nourish all the world.”     —Berowne in Love’s Labor’s Lost   ","categories": ["No Category"],
+        "tags": ["Jekyll","springboot"],
+        "url": "/no%20category/blockquotes/",
+        "teaser": "/assets/img/coding.jpg"
+      }]
